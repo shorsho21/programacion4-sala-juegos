cd4eb965eb5a2f95bd20651bb1c1f80c8852e3c0
Jorge Ezequiel Caballero <87038713+shorsho21@users.noreply.github.com>
2026-05-18 08:42:58 -0300

    sprint-1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { BienvenidaComponent } from './bienvenida.component';
+import { LoginComponent } from './login.component';
+import { RegistroComponent } from './registro.component';
+import { QuienSoyComponent } from './quien-soy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: BienvenidaComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'quien-soy', component: QuienSoyComponent },
+  { path: '**', redirectTo: 'home' }
+];
